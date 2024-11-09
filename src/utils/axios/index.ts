79eb6660bd9455bac03ccbddeb8c09fea33b1bbd
dunzae <dunzae99@gmail.com>
2024-11-09 10:33:41 +0900

    @Chore .env.production에서 중복되는 값 제거

--- src/utils/axios/index.ts
+++ src/utils/axios/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-	baseURL: import.meta.env.PROD ? `http://${import.meta.env.VITE_SERVER_ADDRESS}:4000/api` : "http://localhost:4000/api/",
+	baseURL: `http://${import.meta.env.VITE_SERVER_URL}:4000/api`,
 });
 
 export default instance;
