4334140cba376e7720d24fb3af55c520d35f7646
dunzae <dunzae99@gmail.com>
2024-11-10 18:43:04 +0900

    @Feat 클라이언트 설정에서 ssl 검증 비활성화

--- src/utils/axios/index.ts
+++ src/utils/axios/index.ts
@@ -1,7 +1,13 @@
 import axios from "axios";
+import https from "https";
+
+const agent = new https.Agent({
+	rejectUnauthorized : false
+})
 
 const instance = axios.create({
 	baseURL: `${import.meta.env.VITE_SERVER_URL}`,
+	httpsAgent : agent,
 });
 
 export default instance;
