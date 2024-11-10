23f630fb85dd1ad10c591a3a7e16c18d84e7a4f4
dunzae <dunzae99@gmail.com>
2024-11-10 18:51:36 +0900

    @Chore https 에이전트 삭제

--- src/utils/axios/index.ts
+++ src/utils/axios/index.ts
@@ -1,13 +1,7 @@
 import axios from "axios";
-import https from "https";
-
-const agent = new https.Agent({
-	rejectUnauthorized : false
-})
 
 const instance = axios.create({
 	baseURL: `${import.meta.env.VITE_SERVER_URL}`,
-	httpsAgent : agent,
 });
 
 export default instance;
